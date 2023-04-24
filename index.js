dab9a47df37a57336cba4107f056621734b0ebcf
AkifHaciyev <akif6563@mail.ru>
2023-04-24 16:02:57 +0400

    js syntax, variables,types,topeOf, stringmethods

---
+++ index.js
@@ -0,0 +1,97 @@
+// console.log('hello');
+// console.log('hello 2');
+// console.log('hello 3');
+// console.log('hello 4');
+
+// alert('error');  dfsdf
+
+// var,let,const
+// var watter = 'watter var';
+// let wAtter = 'watter let';
+// const waTter = 'watter const';
+// watter = 'not a watter';
+// wAtter = 'not a wAtter';
+// waTter = 'not a waTter';
+// console.log(watter);
+// console.log(wAtter);
+// console.log(waTter);
+
+// types///////////////////////////
+
+// string
+// '',"",``
+// let coffe = '5';
+// console.log(coffe);
+
+// Number
+// let whoyMuchCoffe = 5;
+// console.log(whoyMuchCoffe);
+
+// Boolean
+// let coffeCup = true;
+// console.log(coffeCup);
+// null
+
+// undefined
+// let coffe10;
+// console.log(coffe10);
+// object
+// let object = {
+// 	id: 1,
+// 	coffe: 'black',
+// 	whoyMuchCoffe: 5,
+// };
+
+// console.log(object);
+// symbol
+// const id = Symbol('iaaaaad');
+// console.log(id);
+// bigint
+// -+2^53
+// 100n;
+
+//typeOf
+
+// let watter = true;
+// console.log(typeof watter);
+// console.log(typeof null);
+// console.log(typeof console.log);
+
+//
+// 1. string, 2. number, 3. boolean
+// string
+// let age = 20;
+// console.log('number age typeof:', typeof age);
+// console.log('number age typeof:', typeof String(age));
+// let age1 = 20 + '1';
+// console.log('number age typeof:', typeof age1);
+// number
+// let student = 'hello';
+// console.log('string age typeof:', typeof Number(student));
+// console.log('string age typeof:', typeof +student);
+// console.log('string age typeof:', Number(student));
+//boolean
+
+// console.log('hello', Boolean('hello'));
+// console.log(5, Boolean(5));
+//null,undefined,0,NaN,''////
+// console.log('null', Boolean(null));
+// console.log('undefined', Boolean(undefined));
+// console.log('NaN', Boolean(NaN));
+// console.log('', Boolean(''));
+
+// String methods
+// let name = 'Ali+Sara             ';
+// let age = 19;
+
+// console.log(name + ' ' + age + ' ' adsa) ;
+// console.log(`${name} ${age}`);
+// console.log(name.length);
+// console.log(name.charAt(0));
+// console.log(name[0]);
+// console.log(name.toUpperCase());
+// console.log(name.indexOf('l'));
+// console.log(name.slice(0, 3));
+// console.log(name.replace('A', 'U'));
+// console.log(name.replaceAll('a', 'U'));
+// console.log(name.trim());
